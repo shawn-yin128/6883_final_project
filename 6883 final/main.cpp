03b344f334cbd829b73227fdb9138a7413c0d71a
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2022-04-23 13:58:33 -0400

    first commit

---
+++ 6883 final/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  6883 final
+//
+//  Created by 殷相杨 on 4/23/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ DerivedData/6883 final/Build/Intermediates.noindex/6883 final.build/Debug/6883 final.build/Objects-normal/arm64/main.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/yinxiangyang/Desktop/22Spring/6883\ fin\ computing/team\ project/6883\ final/6883\ final/main.cpp
